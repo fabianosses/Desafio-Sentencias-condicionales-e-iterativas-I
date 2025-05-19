7b13ebc1af591ae73bd1bfafe86e42f0756aafdc
fabianosses <fabian.osses@gmail.com>
2025-05-19 03:25:02 +0000

    finaliza script de cachipun.py utilizando sys.argv

--- cachipun.py
+++ cachipun.py
@@ -2,13 +2,40 @@
 import random
 import sys
 
-opciones = ["piedra", "papel", "tijera"]
+# define argumentos que deben elegir al azar
+argumentos = ["piedra", "papel", "tijera"]
 
 # ingreso de argumentos (piedra, papel o tijera)
 usuario = sys.argv[1]
 
-print(f"elegiste {usuario}")
+# Loops
+while True:
 
-computador = random(opciones)
+    print(f"elegiste: {usuario}")
 
-print(f"elegiste {computador}")
+    if usuario == "salir":
+        print("¡FIN!")
+        break
+
+    if usuario not in argumentos:
+        usuario = input("""Argumento no válido ... 
+                        elige: piedra, papel o tijera.
+                        o escribe "salir" para finalizar """)
+        continue
+
+    computador = random.choice(argumentos)
+    print(f"computador: {computador}")
+
+    if usuario == computador :
+        print("empate!!!...")
+
+    elif (usuario == "piedra" and computador == "tijera") or \
+        (usuario == "papel" and computador == "piedra") or \
+        (usuario == "tijera" and computador == "papel"):
+        usuario = input(""" Ganaste!!!... 
+                        elige: piedra, papel o tijera.
+                        o escribe "salir" para finalizar """)
+    else:
+        usuario = input("""Perdiste!!! :( 
+                        elige: piedra, papel o tijera.
+                        o escribe "salir" para finalizar """)
